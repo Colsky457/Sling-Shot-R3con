20ebc117459b812ec8291534611c1d5c69628e97
Colsky457 <collinsskyler272@gmail.com>
2026-09-19 04:37:56 -0700

    Update Python CLI setup and dependencies (#17)
    * update Python CLI setup and dependencies
    
    * keep semaphore limiter capacity positive

--- requirements.sh
+++ requirements.sh
@@ -8,4 +8,13 @@ go install github.com/pry0cc/tew@latest
 go install github.com/projectdiscovery/katana/cmd/katana@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 
+if ! command -v pip3 >/dev/null 2>&1; then
+    if ! python3 -m ensurepip --upgrade; then
+        python3 -c "import urllib.request; urllib.request.urlretrieve('https://bootstrap.pypa.io/get-pip.py', '/tmp/get-pip.py')" && \
+            python3 /tmp/get-pip.py
+    fi
+fi
+
+pip3 install -e ".[dev]"
+
 chmod +x ./shoot.sh
